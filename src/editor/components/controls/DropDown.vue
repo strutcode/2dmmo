b5eaef8bde4bc899a222b292cd31fc864d22b4a0
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-08-21 00:33:47 -0400

    Allow flexible width dropdowns

--- src/editor/components/controls/DropDown.vue
+++ src/editor/components/controls/DropDown.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="dropdown control" @click.stop="toggle">
+  <div class="dropdown control" @click.stop="toggle" :style="{ width }">
     <div ref="header" class="layout-h">
-      <div class="layout-fill">{{ value || placeholder || ''}}</div>
-      <div>
+      <div class="header layout-fill">{{ value || placeholder || ''}}</div>
+      <div class="arrow control">
         <i class="fa fa-angle-down"></i>
       </div>
     </div>
 
-    <div ref="dropdown" class="dropdown-panel control" v-show="isOpen">
+    <div ref="dropdown" class="dropdown-panel control" v-show="isOpen" :style="{ width }">
       <slot></slot>
     </div>
   </div>
@@ -24,8 +24,13 @@
 
     data() {
       return {
+        width: undefined as string | undefined,
         isOpen: false,
       }
+    },
+
+    created() {
+      this.calculateSize()
     },
 
     methods: {
@@ -71,28 +76,53 @@
 
         window.addEventListener('pointerdown', click)
       },
+      calculateSize() {
+        this.isOpen = true
+
+        this.$nextTick(() => {
+          if (
+            this.$refs.header instanceof HTMLElement &&
+            this.$refs.dropdown instanceof HTMLElement
+          ) {
+            const headerSize = this.$refs.header.getBoundingClientRect()
+            const panelSize = this.$refs.dropdown.getBoundingClientRect()
+            this.width = `${Math.max(headerSize.width, panelSize.width)}px`
+          }
+
+          this.isOpen = false
+        })
+      },
     },
   })
 </script>
 
 <style scoped>
   .dropdown {
+    display: inline-block;
     position: relative;
-    min-height: 1.8em;
-    line-height: 1.8em;
-    padding: 0 0.5em;
+    min-height: 2.5em;
+    line-height: 2.5em;
     cursor: default;
+  }
+
+  .header {
+    padding: 0 1.2em;
+  }
+
+  .arrow {
+    width: 2.5em;
+    flex: 0 0 2.5em;
+    text-align: center;
   }
 
   .dropdown-panel {
     position: absolute;
     top: 100%;
     left: 0;
-    width: 100%;
   }
 
   .dropdown-panel > * {
-    padding: 0 0.5em;
+    padding: 0 1.2em;
   }
 
   .dropdown-panel > *:hover {
