c5cdcdd36f6bd2358f95a453d77c1e2a4d9532dc
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-08-24 04:58:09 -0400

    Nametag tweaks

--- src/client/graphics/NameTag.ts
+++ src/client/graphics/NameTag.ts
@@ -4,12 +4,12 @@ export default class NameTag {
   public constructor(public mob: Mobile, public x = 0, public y = 0) {}
 
   public draw(ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle = 'white'
+    ctx.fillStyle = this.mob.sprite === 'player' ? '#00fdff' : 'white'
     ctx.strokeStyle = 'black'
     ctx.lineWidth = 1
     ctx.textAlign = 'center'
     ctx.textBaseline = 'bottom'
-    ctx.font = `bold 3pt Palantino`
+    ctx.font = `3pt Palantino`
     ctx.imageSmoothingEnabled = true
 
     ctx.strokeText(this.mob.name.toUpperCase(), this.x + 8, this.y, 1000)
