4c669df3ecd7480d8eab62f7adcf0311d16eee57
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-08-24 04:48:28 -0400

    Renderer cleanup

--- src/client/graphics/Renderer.ts
+++ src/client/graphics/Renderer.ts
@@ -1,40 +1,11 @@
 import Camera from './Camera'
 import GameState from '../GameState'
 import Mobile from '../entities/Mobile'
+import MobileSprite from './MobileSprite'
 import HitIndicator from './HitIndicator'
-import spritemap from '../../common/data/spritemap'
 import enemies from '../../common/data/enemies'
 import tilesets from '../../common/data/tilesets'
-import Sprite from './Sprite'
-import MobileSprite from './MobileSprite'
-import { resolve } from 'path'
 import sprites from '../../common/data/sprites'
-
-interface Transition {
-  x1: number
-  y1: number
-  x2: number
-  y2: number
-  a: number
-}
-
-interface TextProperties {
-  color?: string
-  size?: number
-  x?: number
-  y?: number
-  hAlign?: CanvasTextAlign
-  vAlign?: CanvasTextBaseline
-}
-
-interface SpriteProperties {
-  sprite: string
-  action: string
-  frame: number
-  x: number
-  y: number
-  flip?: boolean
-}
 
 export default class Renderer {
   private canvas = document.createElement('canvas')
@@ -51,10 +22,6 @@ export default class Renderer {
 
   private camera = new Camera()
   private sprites = new Map<string, MobileSprite>()
-  private assets: Record<string, HTMLImageElement> = {}
-  private frameCounter = new Map<string, number>()
-  private lastAction = new Map<string, string>()
-  private transitionState = new Map<string, Transition>()
   private hitIndicators: HitIndicator[] = []
 
   public constructor(private state: GameState) {
@@ -167,26 +134,6 @@ export default class Renderer {
         sprite.draw(this.context, delta)
       })
 
-    // this.drawTileMap()
-    // ;[...this.state.mobs.values()]
-    //   .sort((a, b) => a.y - b.y)
-    //   .forEach(mob => {
-    //     const { name, sprite, action, flip } = mob
-
-    //     const frame = this.animate(mob, delta)
-    //     const { x, y } = this.transition(mob, delta)
-
-    //     if (mob === this.state.self) {
-    //       this.camera.set(x + 8, y + 8)
-    //     }
-
-    //     this.drawSprite({ sprite, action, frame, x, y, flip })
-    //     this.drawText(name, {
-    //       x: x + 8,
-    //       y: y - 2,
-    //     })
-    //   })
-
     this.hitIndicators.forEach(indicator => {
       indicator.draw(this.context, delta)
     })
@@ -204,165 +151,5 @@ export default class Renderer {
     } else {
       this.camera.scale = window.innerHeight / this.height
     }
-  }
-
-  private animate(mob: Mobile, delta: number) {
-    const last = this.lastAction.get(mob.id)
-    const time =
-      last === mob.action ? this.frameCounter.get(mob.id) || Math.random() : 0
-    const info = (spritemap as any)[mob.sprite][mob.action]
-    const loop = info.loop ?? true
-    const fps = info.fps ?? 4
-    const next = info.next
-
-    this.frameCounter.set(mob.id, time + delta)
-    this.lastAction.set(mob.id, mob.action)
-
-    if (!loop && next && time * fps > info.frames) {
-      mob.action = next
-    }
-
-    return Math.floor(
-      loop ? (time * fps) % info.frames : Math.min(time * fps, info.frames - 1),
-    )
-  }
-
-  private transition(mob: Mobile, delta: number) {
-    const mX = mob.x * 16
-    const mY = mob.y * 16
-    const t = this.transitionState.get(mob.id) ?? {
-      x1: mX,
-      y1: mY,
-      x2: mX,
-      y2: mY,
-      a: 1,
-    }
-
-    if (t.x2 !== mX || t.y2 !== mY) {
-      t.x1 = t.x2
-      t.y1 = t.y2
-      t.x2 = mX
-      t.y2 = mY
-      t.a = 0
-    }
-
-    if (t.a < 1) {
-      t.a += delta * 4
-      if (t.a > 1) t.a = 1
-    }
-
-    this.transitionState.set(mob.id, t)
-
-    return {
-      x: t.x1 + (t.x2 - t.x1) * t.a,
-      y: t.y1 + (t.y2 - t.y1) * t.a,
-    }
-  }
-
-  private drawTile(
-    type: string,
-    sx: number,
-    sy: number,
-    x: number,
-    y: number,
-    flip = false,
-  ) {
-    const scale = this.camera.scale
-    const finalX = Math.floor(
-      window.innerWidth / 2 + (x - this.camera.x) * scale,
-    )
-    const finalY = Math.floor(
-      window.innerHeight / 2 + (y - this.camera.y) * scale,
-    )
-    const size = Math.ceil(16 * scale)
-
-    if (!this.assets[type]) {
-      this.context.fillStyle = 'magenta'
-      this.context.fillRect(finalX, finalY, size, size)
-      return
-    }
-
-    if (flip) {
-      this.flipCtx.clearRect(0, 0, -16, 16)
-      this.flipCtx.setTransform(-1, 0, 0, 1, 0, 0)
-      this.flipCtx.drawImage(
-        this.assets[type],
-        sx * 16,
-        sy * 16,
-        16,
-        16,
-        0,
-        0,
-        -16,
-        16,
-      )
-    }
-
-    this.context.imageSmoothingEnabled = false
-    this.context.drawImage(
-      flip ? this.flipCanvas : this.assets[type],
-      flip ? 0 : sx * 16,
-      flip ? 0 : sy * 16,
-      16,
-      16,
-      finalX,
-      finalY,
-      size,
-      size,
-    )
-  }
-
-  private drawText(text: string, options?: TextProperties) {
-    options = options || {}
-    const x = options.x ?? 0
-    const y = options.y ?? 0
-    const size = options.size ?? 12
-
-    this.context.font = `small-caps bold ${size}pt Arial`
-    this.context.textAlign = options.hAlign || 'center'
-    this.context.textBaseline = options.vAlign || 'top'
-    this.context.fillStyle = options.color ?? 'rgb(124, 240, 255)'
-    this.context.strokeStyle = 'black'
-    this.context.lineWidth = 3
-
-    const finalX = Math.floor(
-      window.innerWidth / 2 + (x - this.camera.x) * this.camera.scale,
-    )
-    const finalY = Math.floor(
-      window.innerHeight / 2 + (y - this.camera.y) * this.camera.scale,
-    )
-
-    this.context.strokeText(text, finalX, finalY)
-    this.context.fillText(text, finalX, finalY)
-  }
-
-  private drawSprite(options: SpriteProperties) {
-    const { sprite, action, frame, x, y, flip } = options
-
-    const { row, frames } = (spritemap as any)[sprite][action]
-    this.drawTile(
-      spritemap[sprite as keyof typeof spritemap]?._asset,
-      Math.floor(frame % frames),
-      row,
-      x,
-      y,
-      flip,
-    )
-  }
-
-  private drawTileMap() {
-    // const map = this.state.map
-    // if (!map) return
-    // let x: number, y: number
-    // for (y = 0; y < map.height; y++) {
-    //   for (x = 0; x < map.width; x++) {
-    //     map.layers.forEach((layer: any[][]) => {
-    //       if (layer[y] && layer[y][x]) {
-    //         const tile = layer[y][x]
-    //         this.drawTile('grassTiles', tile.x, tile.y, x * 16, y * 16)
-    //       }
-    //     })
-    //   }
-    // }
   }
 }
