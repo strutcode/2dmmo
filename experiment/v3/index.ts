9b35d20217443e474c168fd645df14a4d7927a81
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-08-29 19:07:27 -0400

    More work on networking stuff

--- experiment/v3/index.ts
+++ experiment/v3/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 
+const OriginalSymbol = Symbol('original')
 const TypeSymbol = Symbol('type')
 const IdSymbol = Symbol('id')
 const ParentSymbol = Symbol('parent')
@@ -30,9 +31,8 @@ function proxify(value: any, parent?: any, path: string[] = []) {
       const type = parent ? parent[TypeSymbol] : obj[TypeSymbol]
       const id = parent ? parent[IdSymbol] : obj[IdSymbol]
 
-
       let thing = delta
-      path.forEach(key => {
+      path.forEach((key) => {
         thing[key] ??= {}
         thing = thing[key]
       })
@@ -41,7 +41,7 @@ function proxify(value: any, parent?: any, path: string[] = []) {
 
       if (!timer) {
         timer = setTimeout(() => {
-          (parent || obj)[NotSymbol](type, id, delta)
+          ;(parent || obj)[NotSymbol](type, id, delta)
           timer = undefined
           delta = {}
         })
@@ -55,8 +55,8 @@ function proxify(value: any, parent?: any, path: string[] = []) {
 function NetworkSync(...watchedFields: string[]) {
   let gid = 0
 
-  return function<T extends { new (...args: any[]): {} }>(Base: T) {
-    return (function(...args: any[]) {
+  return function <T extends { new (...args: any[]): {} }>(Base: T) {
+    const newType: T = (function (...args: any[]) {
       const inst = new Base(...args) as any
 
       inst[TypeSymbol] = Base.name
@@ -68,13 +68,17 @@ function NetworkSync(...watchedFields: string[]) {
         listeners.push(callback)
       }
       inst[NotSymbol] = (type: string, id: string, delta: any) => {
-        listeners.forEach(listener => {
+        listeners.forEach((listener) => {
           listener(type, id, delta)
         })
       }
 
       return proxify(inst)
     } as unknown) as T
+
+    newType[OriginalSymbol] = Base
+
+    return newType
   }
 }
 
@@ -97,64 +101,104 @@ class Socket {
   onMessage?: (data: any) => void
 }
 
-let clientGid = 0
-class ClientView {
-  private id = clientGid++
+class NetworkView implements Record<string | number | symbol, any> {
+  private registrations = new Map<string, any>()
+  private syncedObjs = new Map<string, any>()
 
-  constructor(socket: Socket) {
-    socket.onMessage = data => {
-      console.dir(`Client ${this.id} recieved:`, data)
+  constructor(private socket: Socket, name: string) {
+    socket.onMessage = (data) => {
+      console.dir(`${name} recieved:`, data)
+      const apply = (obj, target) => {
+        for (let key in obj) {
+          if (typeof obj[key] === 'object') {
+            target[key] ??= obj['length'] ? [] : {}
+            apply(obj[key], target[key])
+          } else {
+            target[key] = obj[key]
+          }
+        }
+
+        return target
+      }
+
+      if (!this.syncedObjs.has(`${data.type}:${data.id}`)) {
+        this.syncedObjs.set(`${data.type}:${data.id}`, {})
+      }
+
+      const ref = this.syncedObjs.get(`${data.type}:${data.id}`)
+      const obj = apply(data.delta, ref)
+
+      if (data.action === 'init') {
+        console.log('init', data.type, data.prop)
+        if (this.registrations.has(data.type)) {
+          console.log('registered as', data.type)
+          Object.setPrototypeOf(
+            obj,
+            this.registrations.get(data.type).prototype,
+          )
+        }
+
+        this[data.prop] = obj
+      }
     }
-  }
-}
 
-class ServerView implements Record<string | number | symbol, any> {
-  constructor(private socket: Socket) {
     return new Proxy(this, {
       set(obj, prop, value) {
-        obj.sync(value)
+        obj.sync(value, prop)
         obj[prop] = value
 
         return true
-      }
+      },
     })
   }
 
-  sync(value: any) {
+  register(type: any) {
+    const proto = type[OriginalSymbol] || type
+    const name = proto.name
+
+    console.log('register', name, proto)
+    this.registrations.set(name, proto)
+  }
+
+  sync(value: any, prop?: string) {
     if (!value[IdSymbol]) {
       console.error(`Not a networked object!`)
       return false
     }
 
     this.socket.send({
-      action: 'create',
-      type: value[TypeSymbol],
+      action: prop ? 'init' : 'create',
+      prop,
+      type: value[TypeSymbol] || Object.getPrototypeOf(value).name,
       id: value[IdSymbol],
       delta: Object.keys(value).reduce((acc, field) => {
         if (value[field][IdSymbol]) {
           acc[field] = `${value[field][TypeSymbol]}:${value[field][IdSymbol]}`
-        }
-        else if (value[FieldsSymbol].length < 1 || value[FieldsSymbol].includes(field)) {
+          this.sync(value[field])
+        } else if (
+          value[FieldsSymbol].length < 1 ||
+          value[FieldsSymbol].includes(field)
+        ) {
           acc[field] = value[field]
         }
 
         return acc
-      }, {} as any)
+      }, {} as any),
     })
 
     const observe = (target: Record<string | typeof RegSymbol, any>) => {
-      Object.values(target).forEach(value => {
+      Object.values(target).forEach((value) => {
         if (typeof value === 'object' && value[IdSymbol]) {
           observe(value)
         }
       })
-      
+
       target[RegSymbol]((type: string, id: string, delta: any) => {
         this.socket.send({
           action: 'update',
           type,
           id,
-          delta
+          delta,
         })
       })
     }
@@ -165,7 +209,7 @@ class ServerView implements Record<string | number | symbol, any> {
 
 @NetworkSync('items')
 class Container {
-  items = ['foo', 'bar']
+  items = []
 }
 
 @NetworkSync('x', 'y', 'hp', 'inventory')
@@ -183,11 +227,23 @@ class HeroList {
   add(hero: Hero) {
     this.list.push(hero)
   }
+
+  map(callback: (hero: Hero) => any): any[] {
+    return this.list.map(callback)
+  }
 }
 
-class MyView extends ServerView {
+class MyView extends NetworkView {
   hero = new Hero()
   players = new HeroList()
+
+  constructor(...args: ConstructorParameters<typeof NetworkView>) {
+    super(...args)
+
+    this.register(Container)
+    this.register(Hero)
+    this.register(HeroList)
+  }
 }
 
 class Server {
@@ -199,20 +255,22 @@ class Server {
     socket.connectedTo = client
     client.connectedTo = socket
 
-    const view = new MyView(client)
+    const view = new MyView(client, 'server')
 
-    this.clients.forEach(client => {
+    this.clients.forEach((client) => {
       view.players.add(client.hero)
       client.players.add(view.hero)
     })
+
     this.clients.push(view)
     console.log('client connected')
   }
 }
 
+let n = 0
 class Client {
   private socket = new Socket()
-  public view = new ClientView(this.socket)
+  public view = new MyView(this.socket, `client ${n++}`)
 
   connect(server: Server) {
     server.connection(this.socket)
@@ -224,29 +282,92 @@ const server = new Server()
 
 const ClientComponent = Vue.extend({
   render(h): Vue.VNode {
-    return h('div', 'Hello world')
+    const { hero, players } = this.client.view
+    const playerBox = { position: 'absolute', width: '16px', height: '16px' }
+
+    return h(
+      'div',
+      {
+        attrs: {
+          tabIndex: -1,
+        },
+        style:
+          'display: inline-block; position: relative; background: lightgrey; width: 160px; height: 160px',
+        on: {
+          keydown: (ev: KeyboardEvent) => {
+            switch (ev.key) {
+              case 'w':
+                this.client.view.hero.y--
+                break
+              case 's':
+                this.client.view.hero.y++
+                break
+              case 'a':
+                this.client.view.hero.x--
+                break
+              case 'd':
+                this.client.view.hero.x++
+                break
+            }
+          },
+        },
+      },
+      [
+        h('div', {
+          style: {
+            ...playerBox,
+            background: 'orange',
+            top: `${hero.y * 16}px`,
+            left: `${hero.x * 16}px`,
+          },
+        }),
+        ...players.map((player) =>
+          h('div', {
+            style: {
+              ...playerBox,
+              background: 'purple',
+              top: `${player.y * 16}px`,
+              left: `${player.x * 16}px`,
+            },
+          }),
+        ),
+      ],
+    )
   },
   data() {
     return {
-      client: new Client()
+      client: new Client(),
     }
   },
   created() {
     this.client.connect(server)
-  }
+  },
 })
 
 new Vue({
   el: document.body,
   render(h): Vue.VNode {
     return h('div', [
-      h('div', this.clients.map(() => h(ClientComponent))),
-      h('button', { on: { click: () => { this.clients.push(Math.random().toString(36)) } } }, 'Add Client')
+      h(
+        'div',
+        this.clients.map(() => h(ClientComponent)),
+      ),
+      h(
+        'button',
+        {
+          on: {
+            click: () => {
+              this.clients.push(Math.random().toString(36))
+            },
+          },
+        },
+        'Add Client',
+      ),
     ])
   },
   data() {
     return {
-      clients: [] as string[]
+      clients: [] as string[],
     }
-  }
+  },
 })
