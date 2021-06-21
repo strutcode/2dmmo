9691cfd72c5f95262089994e5d63408a4a4d7edc
Tristan Shelton <tristan.m.shelton@gmail.com>
2021-06-21 04:50:58 +0000

    Update web to allow name entry

---
+++ web/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form method="GET" action="/game">
+      <div>
+        <label for="name">Display Name</label>
+      </div>
+      <div>
+        <input type="text" name="name" value="Soandso" />
+      </div>
+      <div>
+        <button type="submit">Launch Game</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({})
+</script>
---
+++ web/src/index.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+import App from './App.vue'
+
+new Vue({
+  el: document.getElementById('app'),
+  render: (h) => h(App),
+})
