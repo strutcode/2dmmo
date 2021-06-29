3efdffb02b8d39cc78dc32339f7803c55b12accc
Tristan Shelton <tristan.m.shelton@gmail.com>
2021-06-29 18:06:48 -0400

    Clean up miscellaneous project problems

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,6 +3,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
-  el: document.getElementById('app'),
+  el: document.getElementById('app') as HTMLDivElement,
   render: (h) => h(App),
 })
