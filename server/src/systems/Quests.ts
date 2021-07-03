d4895a5b65c8c6bd3f89cd2d5a6610d47ac4a8ce
Tristan Shelton <tristan.m.shelton@gmail.com>
2021-07-03 16:50:29 -0400

    Prep work for quests

---
+++ server/src/systems/Quests.ts
@@ -0,0 +1,9 @@
+import System from '../../../common/engine/System'
+
+export default class Quests extends System {
+  public update() {
+    // TODO: Load quests
+    // TODO: Tie in to population and resource systems somehow
+    // TODO: Query quests to assign to players
+  }
+}
