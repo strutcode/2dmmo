65e8c598782a82ec20d5f162b648f61e9d5442b8
Tristan Shelton <tristan.m.shelton@gmail.com>
2021-07-04 15:28:38 -0400

    Simple NPCs

--- server/src/systems/PopulationManager.ts
+++ server/src/systems/PopulationManager.ts
@@ -9,9 +9,9 @@ import RandomWalk from '../behaviors/RandomWalk'
 export default class PopulationManager extends System {
   public update() {
     // TODO: need a better method
-    const deer = this.engine
-      .getAllComponents(Mobile)
-      .filter((mob) => mob.name === 'Deerling')
+    const mobs = this.engine.getAllComponents(Mobile)
+    const deer = mobs.filter((mob) => mob.name === 'Deerling')
+    const npcs = mobs.filter((mob) => mob.sprite === 'monk')
 
     // If there are fewer than 10 deer
     if (deer.length < 10) {
@@ -30,5 +30,46 @@ export default class PopulationManager extends System {
         [Brain, { behaviors: [RandomWalk] }],
       ])
     }
+
+    // Generate some NPCs
+    if (npcs.length < 3) {
+      this.engine.createEntity([
+        [Mobile, { name: this.generateName(), sprite: 'monk' }],
+        Input,
+        Affectable,
+        [
+          TilePosition,
+          {
+            x: Math.floor(Math.random() * 31) - 13,
+            y: Math.floor(Math.random() * 31) - 13,
+          },
+        ],
+      ])
+    }
+  }
+
+  private namePieces = {
+    start: ['fa', 'da', 'a', 'u', 'e', 'fo', 'ke', 'na', 'vi', 'fal', 'dal'],
+    mid: ['be', 'na', 'ne', 'le', 'lo', 'ke', 'di', 'da', 'no', 'ri'],
+    end: ['rt', 'nt', 'rr', 'n', 'o', 'd', 'm', 'mar'],
+  }
+  private generateName() {
+    const arrayRand = (arr: any[]) =>
+      arr[Math.floor(Math.random() * arr.length)]
+    const upperCase = (input: string) =>
+      input[0].toUpperCase() + input.substr(1)
+
+    const start = () => upperCase(arrayRand(this.namePieces.start))
+    const mid = () => arrayRand(this.namePieces.mid)
+    const end = () => arrayRand(this.namePieces.end)
+    const name = () => {
+      if (Math.random() < 0.7) {
+        return `${start()}${mid()}${end()}`
+      } else {
+        return `${start()}${mid()}${mid()}${end()}`
+      }
+    }
+
+    return `${name()} ${name()}`
   }
 }
