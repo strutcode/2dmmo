db727f9ae0128983b4c9cf0aeb1c9704d70387c0
Tristan Shelton <tristan.m.shelton@gmail.com>
2021-08-09 18:23:54 -0400

    Add keyboard functionality

--- editor/src/ui/QuestEditor.vue
+++ editor/src/ui/QuestEditor.vue
@@ -80,8 +80,26 @@
         this.editPos.y = ev.y
       })
 
-      console.log(editor)
+      editor.on('keydown', (e) => {
+        switch (e.code) {
+          case 'Delete':
+            editor.selected.each((n) => editor.removeNode(n))
+            break
+
+          case 'Space':
+            let rect = editor.view.container.getBoundingClientRect()
+            let event = new MouseEvent('contextmenu', {
+              clientX: rect.left + rect.width / 2,
+              clientY: rect.top + rect.height / 2,
+            })
+
+            editor.trigger('contextmenu', { e: event, view: editor.view })
+            break
+        }
+      })
+
       this.editor = editor
+      window.editor = editor
     },
 
     methods: {
