6e42927c08d38eba7619b5e98ee52988bea80dc9
Tristan Shelton <tristan.m.shelton@gmail.com>
2021-10-24 21:32:18 -0400

    Quest updates

---
+++ server/src/systems/Heirloom.ts
@@ -0,0 +1,7 @@
+import System from '../../../common/engine/System'
+
+/**
+ * This system should eventually handle items being passed down from
+ * one generation of player to the next
+ */
+export default class Heirloom extends System {}
